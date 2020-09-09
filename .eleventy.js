0061670182e88c8cef173907d0076f67251bbe73
Seb Jones <jones.seb95@googlemail.com>
2020-09-09 09:46:24 +0100

    Run blog post excerpts through a markdown parser.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
+let md = require('markdown-it')();
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight);
+    
+    eleventyConfig.addNunjucksFilter("md", (value) => { 
+        return md.renderInline(value);
+    });
 };
