2c201ecb452f3e20bfe00c786a2aaef36118dba0
Seb Jones <jones.seb95@googlemail.com>
2020-09-09 10:59:21 +0100

    Parse blog dates with moment js.

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,16 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-let md = require('markdown-it')();
+const md = require("markdown-it");
+const moment = require("moment");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight);
     
     eleventyConfig.addNunjucksFilter("md", (value) => { 
-        return md.renderInline(value);
+        return md().renderInline(value);
+    });
+
+    eleventyConfig.addNunjucksFilter("date", (value) => { 
+        return moment(value).format("Do MMMM YYYY");
     });
 };
