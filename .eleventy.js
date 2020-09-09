aee4ebf9780499c52a71591c64bd36ffaef09c63
Seb Jones <jones.seb95@googlemail.com>
2020-09-09 11:25:44 +0100

    List tag names in blog index page.

--- .eleventy.js
+++ .eleventy.js
@@ -5,12 +5,32 @@ const moment = require("moment");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight);
-    
+
     eleventyConfig.addNunjucksFilter("md", (value) => { 
         return md().renderInline(value);
     });
 
     eleventyConfig.addNunjucksFilter("date", (value) => { 
         return moment(value).format("Do MMMM YYYY");
+    });
+
+    eleventyConfig.addCollection("tagList", function (collection) {
+        let tagSet = new Set();
+
+        collection.getAll().forEach(function(item) {
+            if ("tags" in item.data) {
+                let tags = item.data.tags;
+
+                tags = tags.filter(function (item) {
+                    return !["all", "blog"].includes(item);
+                });
+
+                for (const tag of tags) {
+                    tagSet.add(tag);
+                }
+            }
+        });
+
+        return [...tagSet];
     });
 };
