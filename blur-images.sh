3896651fbf9da98e064f3b4dac741d8cd8d0ad7b
Seb Jones <jones.seb95@googlemail.com>
2020-09-11 19:39:16 +0100

    Remove unneeded file.

--- blur-images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./images/blurred
-
-find images -maxdepth 1 -type f | \
-    sed -E -e 's@(^images/)(.*$)@\0 \1blurred/\2@g' | \
-    xargs --verbose -P 0 -L 1 convert -gaussian-blur 75x25 -brightness-contrast -10
