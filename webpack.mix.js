e3a78a387d201ba695c315564d1c5e6e3d5e0d47
Seb Jones <jones.seb95@googlemail.com>
2020-10-03 22:28:01 +0100

    Update npm packages and use npm to package alpine.

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ let mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
 mix.sass('sass/app.scss', '_site/')
+   .js('galaxy/scripts.js', '_site/galaxy/scripts.js')
    .options({
        processCssUrls: false,
        postCss: [ tailwindcss('./tailwind.config.js') ]
