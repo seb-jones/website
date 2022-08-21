d12d3bf7ded75cd57ba3b7922d33cd58ec258a90
Seb Jones <jones.seb95@googlemail.com>
2022-08-21 16:49:10 +0100

    Add 404 page

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
+                fourOhFour: resolve(__dirname, '404.html'),
                 nested: resolve(__dirname, 'potholderz/index.html')
             }
         }
