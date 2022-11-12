720eaeffed745e076d94d3c179cae7ec65248c49
Seb Jones <jones.seb95@googlemail.com>
2022-11-12 22:31:40 +0000

    Convert a geojson Polygon to indexed vertices and render

--- globe/convert-geojson-to-object.js
+++ globe/convert-geojson-to-object.js
@@ -1,12 +1,14 @@
 import fs from 'fs'
+import earcut from 'earcut'
+import _ from 'lodash'
 
-const geojson = JSON.parse(fs.readFileSync('./ne_10m_populated_places_simple.json', 'utf8'))
+//
+// Convert 'Populated Places' GeoJSON to a JavaScript object
+//
 
-const vertices = geojson.features.filter(
-    // feature => feature.properties.featurecla === 'Admin-0 capital'
-    // Features in the uk
-    feature => true
-).map(
+let geojson = JSON.parse(fs.readFileSync('./ne_10m_populated_places_simple.json', 'utf8'))
+
+let vertices = geojson.features.map(
     feature => feature.geometry.coordinates // [longitude, latitude]
 ).map(
     // Convert longitude and latitude to x, y, z coordinates
@@ -21,7 +23,7 @@ const vertices = geojson.features.filter(
     }
 ).flat(1)
 
-const data = {
+let data = {
     "metadata":{
         "position":505,
         "type":"BufferGeometry",
@@ -40,3 +42,54 @@ const data = {
 }
 
 fs.writeFileSync('./points.json', JSON.stringify(data))
+
+
+//
+// Convert 'Countries' GeoJSON to a JavaScript object
+//
+
+geojson = JSON.parse(fs.readFileSync('./ne_10m_admin_0_countries.json', 'utf8'))
+
+let feature = geojson.features.find(feature => feature.properties.NAME === 'Poland')
+
+let geometry = earcut.flatten(feature.geometry.coordinates);
+
+let indices = earcut(geometry.vertices, geometry.holes, geometry.dimensions);
+
+vertices = _.chunk(geometry.vertices, 2).map(
+    // Convert longitude and latitude to x, y, z coordinates
+    ([longitude, latitude]) => {
+        const phi = (90 - latitude) * Math.PI / 180
+        const theta = (180 - longitude) * Math.PI / 180
+        return [
+            Math.sin(phi) * Math.cos(theta),
+            Math.cos(phi),
+            Math.sin(phi) * Math.sin(theta)
+        ]
+    }
+).flat(1)
+
+data = {
+    "metadata":{
+        "position":505,
+        "type":"BufferGeometry",
+        "generator":"io_three",
+        "version":3
+    },
+    "data":{
+        "attributes":{
+            "position":{
+                "itemSize":3,
+                "type":"Float32Array",
+                "array": vertices
+            }
+        },
+        "index":{
+            "itemSize":1,
+            "type":"Uint16Array",
+            "array": indices
+        }
+    }
+}
+
+fs.writeFileSync('./countries.json', JSON.stringify(data))
--- globe/main.js
+++ globe/main.js
@@ -9,6 +9,7 @@ import { BloomPass } from 'three/examples/jsm/postprocessing/BloomPass.js';
 let camera, scene, renderer, composer;
 
 let points = null;
+let mesh = null;
 
 init();
 animate();
@@ -18,7 +19,7 @@ function init() {
 
     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
 
-    camera.position.z = 2.5;
+    camera.position.z = 2.0;
 
     scene = new THREE.Scene();
 
@@ -26,14 +27,23 @@ function init() {
     const loader = new THREE.BufferGeometryLoader();
     loader.load('/globe/points.json', function ( geometry ) {
         points = new THREE.Points( geometry, new THREE.PointsMaterial( { color: 0x00ff00, size: .025 } ) );
-        scene.add( points );
+        // scene.add( points );
+    });
+
+    // Load countries geometry
+    loader.load('/globe/countries.json', function ( geometry ) {
+        mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: 0xffff00 } ) );
+        scene.add(mesh);
     });
 
     // Sphere representing the Earth
     const geometry = new THREE.SphereGeometry( 0.99, 32, 32 );
     const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
     const sphere = new THREE.Mesh( geometry, material );
-    scene.add( sphere );
+    // scene.add( sphere );
+
+    scene.rotation.y = 1.2;
+    scene.rotation.x = 0.6;
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
@@ -66,17 +76,15 @@ function onWindowResize() {
 
 function onKeyDown( event ) {
     switch ( event.keyCode ) {
-        case 38: /*up*/ points.rotation.x -= 0.1; break;
-        case 40: /*down*/ points.rotation.x += 0.1; break;
+        case 38: /*up*/ scene.rotation.x -= 0.1; break;
+        case 40: /*down*/ scene.rotation.x += 0.1; break;
     }
 }
 
 function animate() {
     requestAnimationFrame(animate);
 
-    if (points) {
-        points.rotation.y += 0.001;
-    }
+    scene.rotation.y += 0.001;
 
     renderer.clear();
     composer.render( 0.01 );
