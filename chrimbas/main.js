65c6a5e4c7d607328ab3b1b7b1fa08e04090dd34
Seb Jones <seb.jones@enovate.co.uk>
2022-12-25 08:47:06 +0000

    Set up new 'chrimbas' page with basic static mesh loading

---
+++ chrimbas/main.js
@@ -0,0 +1,103 @@
+import './style.css';
+
+import * as THREE from 'three';
+
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+let clock, scene, camera, renderer, stats;
+
+init();
+
+function init() {
+
+    clock = new THREE.Clock();
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x88ccee );
+    scene.fog = new THREE.Fog( 0x88ccee, 0, 50 );
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.rotation.order = 'YXZ';
+
+    const fillLight1 = new THREE.HemisphereLight( 0x4488bb, 0x002244, 0.5 );
+    fillLight1.position.set( 2, 1, 1 );
+    scene.add( fillLight1 );
+
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
+    directionalLight.position.set( - 5, 25, - 1 );
+    directionalLight.castShadow = true;
+    directionalLight.shadow.camera.near = 0.01;
+    directionalLight.shadow.camera.far = 500;
+    directionalLight.shadow.camera.right = 30;
+    directionalLight.shadow.camera.left = - 30;
+    directionalLight.shadow.camera.top	= 30;
+    directionalLight.shadow.camera.bottom = - 30;
+    directionalLight.shadow.mapSize.width = 1024;
+    directionalLight.shadow.mapSize.height = 1024;
+    directionalLight.shadow.radius = 4;
+    directionalLight.shadow.bias = - 0.00006;
+    scene.add( directionalLight );
+
+    const container = document.getElementById( 'container' );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.VSMShadowMap;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    container.appendChild( renderer.domElement );
+
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.top = '0px';
+    container.appendChild( stats.domElement );
+
+    window.addEventListener( 'resize', onWindowResize );
+
+    const loader = new GLTFLoader();
+
+    loader.load( '/models/chrimbas.glb', function ( gltf ) {
+
+        const model = gltf.scene;
+
+        model.traverse( function ( child ) {
+
+            if ( child.isMesh ) {
+
+                child.castShadow = true;
+
+            }
+
+        } );
+
+        scene.add( model );
+
+    } );
+
+    animate();
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    const delta = clock.getDelta();
+
+    renderer.render( scene, camera );
+
+    stats.update();
+
+}
